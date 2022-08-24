e82ed4339d58a5b7b6ecab267b875544aa631509
Galass94 <109736913+Galass94@users.noreply.github.com>
2022-08-24 13:43:24 +0200

    changed text and added wait at the end

--- DivOrbCalc.py
+++ DivOrbCalc.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-divOrbCount = float(input("Divine Orb Calculator\nHow many orbs do you need to pay in chaos?\n").replace(",", "."))
+divOrbCount = float(input("Divine Orb Calculator\nHow many Divine Orbs do you want to convert to Chaos Orbs?\n").replace(",", "."))
 
 values = requests.get("https://poe.ninja/api/data/currencyoverview?league=Kalandra&type=Currency").text
 valuesJson = json.loads(values)
@@ -10,3 +10,4 @@ for i in valuesJson["lines"]:
     if i["detailsId"] == "divine-orb":
         divOrbValue = i["receive"]["value"]
 print(f"That's {round((divOrbValue)*(divOrbCount))} Chaos Orbs")
+input("Press Enter to exit...")
